74ce33418ae4539df4e16fe43d5dc30f2f1ccb5d
Giuseppe Sollazzo <puntofisso@gmail.com>
2013-05-04 17:00:19 +0100

    API system

--- api/api.php
+++ api/api.php
@@ -1,13 +1,38 @@
 <?php
 
-function getTWFYid($uid) {
-	$result = mySQLquery($query);
-	
+function returnXBRL() {
+$xbrl='
+<?xml version="1.0" encoding="UTF-8"?>
+ 
+
+<xbrli:xbrl
+xmlns:ifrs-gp="http://xbrl.iasb.org/int/fr/ifrs/gp/2005-05-15"
+xmlns:iso4217="http://www.xbrl.org/2003/iso4217"
+xmlns:xbrli="http://www.xbrl.org/2003/instance"
+xmlns:xbrll="http://www.xbrl.org/2003/linkbase"
+xmlns:xlink="http://www.w3.org/1999/xlink">
+ 
+    <xbrll:schemaRef xbrll:href="http://www.org.com/xbrl/taxonomy" xlink:type="simple"/>
+    <ifrs-gp:OtherOperatingIncomeTotalByNature contextRef="J2004" 
+        decimals="0" unitRef="EUR">10430000000</ifrs-gp:OtherOperatingIncomeTotalByNature>
+    <xbrli:context id="BJ2004">
+        <xbrli:entity>
+            <xbrli:identifier scheme="www.iqinfo.com/xbrl">It\'s the LAW!!!</xbrli:identifier>
+        </xbrli:entity>
+        <xbrli:period>
+            <xbrli:instant>2004-01-01</xbrli:instant>
+        </xbrli:period>
+    </xbrli:context>
+</xbrli:xbrl>
+';
+return $xbrl;
 }
 
-function getAllUniqueKeywords($seed) {
+function returnSomething($var) {
 	
 	$answer = array();
+	$query = "";
+	$result = mySQLquery($query);
 	while($row = mysql_fetch_assoc( $result )) {
 		$answer[] = $row['keyword']; // this is a push operation. PHP!
 	}
@@ -16,17 +41,17 @@ function getAllUniqueKeywords($seed) {
 }
 
 function mySQLquery($query) {
-	include('../../config/config.php');
-mysql_connect($DB_HOST, $DB_USER, $DB_PASS) or die(mysql_error()." | HOST ".$DB_HOST." USER ".$DB_USER." PASS ".$DB_PASS ." NAME ".$DB_NAME." | Error on connect ");
-mysql_select_db($DB_NAME) or die(mysql_error()." | HOST ".$DB_HOST." USER ".$DB_USER." PASS ".$DB_PASS ." NAME ".$DB_NAME." | Error on select db ");
+	include('config.php');
+	mysql_connect($DB_HOST, $DB_USER, $DB_PASS) or die(mysql_error()." | HOST ".$DB_HOST." USER ".$DB_USER." PASS ".$DB_PASS ." NAME ".$DB_NAME." | Error on connect ");
+	mysql_select_db($DB_NAME) or die(mysql_error()." | HOST ".$DB_HOST." USER ".$DB_USER." PASS ".$DB_PASS ." NAME ".$DB_NAME." | Error on select db ");
 
-$result = mysql_query("$query") or die(mysql_error()." | HOST ".$DB_HOST." USER ".$DB_USER." PASS ".$DB_PASS ." NAME ".$DB_NAME. " | Error on query " );  
+	$result = mysql_query("$query") or die(mysql_error()." | HOST ".$DB_HOST." USER ".$DB_USER." PASS ".$DB_PASS ." NAME ".$DB_NAME. " | Error on query " );  
 
-// keeps getting the next row until there are no more to get
-#while($row = mysql_fetch_array( $result )) {
-#	// Print out the contents of each row into a table
-#}
-return $result; 
+	// keeps getting the next row until there are no more to get
+	#while($row = mysql_fetch_array( $result )) {
+	#	// Print out the contents of each row into a table
+	#}
+	return $result; 
 }
 
 ?>
--- api/index.php
+++ api/index.php
@@ -2,31 +2,98 @@
 include("api.php");
 require("Toro.php");
 
+// /
 class RootHandler {
     function get() {
-      echo "Follow The Data";
+      echo "Follow The Data API v0.9. 04052013<br/>";
+      echo "Available endpoints:<br/>";
+      echo "- /: general info, version, date<br/>";
+      echo "- /country: info by country<br/>";
+      echo "- /company: info by company<br/>";
+      echo "- /value: search country and companies by value range<br/>"; 
     }
 }
 
-
-// $keywords = comma-separated list of keywords
-class searchMPHandler {
-    function get($keywords) {
-      //header('Access-Control-Allow-Origin: *');
-      print searchMP($keywords);
-    }
-}
-
-
-
-class getTWFYidHandler {
+// /xbml
+class getXBRLHandler {
     function get($uid) {
       //header('Access-Control-Allow-Origin: *');
-      print getTWFYid($uid);
+      print returnXBRL();
     }
 }
+
+// /country
+class getCountryHandler {
+    function get() {
+      //header('Access-Control-Allow-Origin: *');
+      print "Countries available";
+    }
+}
+
+class getCountryDataHandler {
+    function get($country) {
+      //header('Access-Control-Allow-Origin: *');
+      print "All values for $country";
+    }
+}
+
+class getCountryValueHandler {
+    function get($country,$param) {
+      //header('Access-Control-Allow-Origin: *');
+      print "Value of $param for $country";
+    }
+}
+
+// /company
+class getCompanyHandler {
+    function get() {
+      //header('Access-Control-Allow-Origin: *');
+      print "Companies available";
+    }
+}
+
+class getCompanyDataHandler {
+    function get($company) {
+      //header('Access-Control-Allow-Origin: *');
+      print "All data for company $company";
+    }
+}
+
+class getCompanyValueHandler {
+    function get($company,$param) {
+      //header('Access-Control-Allow-Origin: *');
+      print "Value of $param for $company";
+    }
+}
+
+// /value 
+class getCountryByValueDataRangeHandler {
+    function get($param, $min, $max) {
+      //header('Access-Control-Allow-Origin: *');
+      print "$param, $min, $max";
+    }
+}
+
+class getCompanyByValueDataRangeHandler {
+    function get($param, $min, $max) {
+      //header('Access-Control-Allow-Origin: *');
+      print "$param, $min, $max";
+    }
+}
+
 Toro::serve(array(
     "/" => "RootHandler",
-    "/country/:alpha" => "searchMPHandler",
-    "/company/:alpha" => "getTWFYidHandler",
+    "/xbrl" => "getXBRLHandler",
+
+    "/country" => "getCountryHandler",
+    "/country/:string" => "getCountryDataHandler",
+    "/country/:string/:string" => "getCountryValueHandler",
+
+    "/company" => "getCompanyHandler",
+    "/company/:string" => "getCompanyDataHandler",
+    "/company/:string/:string" => "getCompanyValueHandler",
+
+    "/value/country/:string/:number/:number" => "getCountryByValueDataRangeHandler",
+    "/value/company/:string/:number/:number" => "getCompanyByValueDataRangeHandler",
+
 ));
